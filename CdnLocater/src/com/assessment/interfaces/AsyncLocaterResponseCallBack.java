9acd06d96379b471058f3a2c7c06577ac39f979b
nadirhussain1 <nadirhussain1@gmail.com>
2020-01-04 10:56:50 +0500

    Filtering repo
    Removed smaller projects

--- CdnLocater/src/com/assessment/interfaces/AsyncLocaterResponseCallBack.java
+++
@@ -1,7 +0,0 @@
-package com.assessment.interfaces;
-
-import com.assessment.model.Server;
-
-public interface AsyncLocaterResponseCallBack {
-	public void onResult(String resultCode,Server server);
-}
